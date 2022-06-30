221a4ceba9a30eaf4738a95d4e8703fe265b8d04
jb lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2022-06-30 09:37:12 +0200

    wip: using  cap instead of arpjs npm package to send arp request

--- .npm.scripts/ops/utils/arp/scanner.js
+++ .npm.scripts/ops/utils/arp/scanner.js
@@ -3,7 +3,9 @@ var arp = require('arpjs');
 const NEIGHBOORHOOD_CENTER = process.env.POKUS_NEIGHBOORHOOD_CENTER_IPADDR || '192.168.98.202'
 let src_ip_addr = '10.105.50.100'
 
-//
+// this will be like sending a ping to a given IP address
+// I need to run a lot of pings as quickly as possible, so in multithreading : https://medium.com/@mohllal/node-js-multithreading-a5cd74958a67
+// ---
 arp.send({
     'op': 'request',
     'src_ip': '10.105.50.100', // that 's the IPv4 Address of the sender machine net interface (the TP Link WIfi Network Adapter in my case)
@@ -11,3 +13,17 @@ arp.send({
     'src_mac': '8f:3f:20:33:54:44', /// that's the MAC address of the sender machine net interface (the TP Link WIfi Network Adapter in my case)
     'dst_mac': 'ff:ff:ff:ff:ff:ff' //this FFFFFFFFF adddress is the broadcast address, it shall never change, it just means send to everyone on same network (L2).
     })
+
+
+/**
+ * A few examples showing that other tools use the 
+ * same technique (pinging all IP Addresses) to discover the IP Address assigned to a given MAC Address by a DHCP server : 
+ * 
+ * https://www.comparitech.com/net-admin/how-to-find-an-ip-address-using-a-mac-address/
+ * 
+ * 
+ * Ok there seem to be problems using arpjs on Window, and that 
+ *  -> https://github.com/mscdex/cap is better integrated to Windows, and 
+ *  -> https://github.com/mscdex/cap allows sending ARP request, just as arpjs! (i tried installing npm i --save cap it works without error !)
+ *  
+ */
--- .npm.scripts/ops/utils/preps.sh
+++ .npm.scripts/ops/utils/preps.sh
@@ -4,11 +4,13 @@
 
 choco install -y packer
 choco install -y nodejs
+# NOT YET AUTOMATED : we also need VERY IMPORTANT FOR NODEJS NODE-GGYP ERRORS : to install VS Studio Build Tools for C++
 choco install -y vscode
 if [ -f $(which python) ]; then 
   rm $(which python)
 fi;
 
+# -- install python3 on windows is for node-gyp, a,d node-gyp required to use 'arpjs' npm package
 if [ -f $(which python3) ]; then 
   rm $(which python3)
 fi;
@@ -17,6 +19,32 @@ choco install -y python3
 
 export PY3_FOLDER_TO_ADD_IN_PATH=$(ls -alh /C/ | grep Python3 | awk '{print $NF}' | awk -F '/' '{print $1}')
 /C/${PY3_FOLDER_TO_ADD_IN_PATH}/python.exe --version
+
+
+# ----
+# Now Install lib pcap : also required by 'arpjs' npm package
+#
+# -> see : https://stackoverflow.com/questions/41762868/fatal-error-c1083-cannot-open-include-file-pcap-h
+# -> see : https://www.winpcap.org/devel.htm
+# -> see : https://github.com/node-pcap/node_pcap/issues/95#issuecomment-102388747
+# -> see : https://github.com/node-pcap/node_pcap/issues/95
+curl -LO https://www.winpcap.org/install/bin/WpdPack_4_1_2.zip
+
+mkdir -p ./.here_i_have_it/
+
+unzip -d ./.here_i_have_it/ WpdPack_4_1_2.zip
+
+
+mkdir -p $LOCALAPPDATA/Microsoft/MSBuild/Include
+mkdir -p $LOCALAPPDATA/Microsoft/MSBuild/Lib
+
+cp -fR ./.here_i_have_it/WpdPack/Include/* $LOCALAPPDATA/Microsoft/MSBuild/Include
+cp -fR ./.here_i_have_it/WpdPack/Lib/* $LOCALAPPDATA/Microsoft/MSBuild/Lib
+
+
+# ---
+# Shell profile addon 
+
 
 if [ -f ./.pokus_profile ]; then
   rm ./.pokus_profile
@@ -32,6 +60,7 @@ touch ~/.profile
 touch ~/.bash_profile
 touch ~/.bashrc
 
+# append profile addon
 cat ./.pokus_profile | tee -a ~/.profile
 cat ./.pokus_profile | tee -a ~/.bash_profile
 cat ./.pokus_profile | tee -a ~/.bashrc
